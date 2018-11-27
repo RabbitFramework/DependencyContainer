863d85963e740c70431b7cc866fef7cd2950f718
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-11-27 18:30:45 +0100

    [Add] EntityExceptions

---
+++ src/Entities/EntityException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 25/11/2018
+ * Time: 14:06
+ */
+
+namespace Xirion\DependencyInjector\Entities;
+
+
+class EntityException extends \Exception
+{
+
+}
